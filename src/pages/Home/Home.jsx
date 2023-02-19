37fcae0344d3cd9c1980b3f916a3d42265be0362
Aymen Isfiaya <aymen.isfiaya@gmail.com>
2023-02-19 10:20:08 +0100

    refactor : home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../../supabase/init";
+import EditExpenseModal from "../../components/EditExpenseModal";
+import TableExpenses from "../../components/TableExpenses";
+import expensesLogic from "../../logic/expensesLogic";
+import { useActions } from "kea";
 
 function Home() {
+  const { saveExpense, loadExpenses } = useActions(expensesLogic);
+  const [formValues, setFormValues] = useState({
+    claimerName: "",
+    expenseDate: "",
+    description: "",
+    amount: 0,
+    approved: false,
+  });
+
+  // MODAL
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      claimer_name: formValues.claimerName,
+      expense_date: formValues.expenseDate,
+      description: formValues.description,
+      amount: formValues.amount,
+      approved: formValues.approved,
+    };
+    saveExpense(data);
+    loadExpenses();
+  }
+
+  const handleEditExpense = (expense) => {
+    setSelectedExpense(expense);
+    setShowEditModal(true);
+  };
+
+  const handleSaveExpense = async (updatedExpense) => {
+    let { data, error } = await supabase
+      .from("expenses")
+      .update(updatedExpense)
+      .eq("id", selectedExpense.id);
+
+    if (error) console.log("Error updating expense:", error.message);
+    else {
+      setExpenses(
+        expenses.map((expense) =>
+          expense.id === data[0].id ? { ...expense, ...data[0] } : expense
+        )
+      );
+      setShowEditModal(false);
+      setSelectedExpense(null);
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setShowEditModal(false);
+    setSelectedExpense(null);
+  };
+  const handleFormChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+
+    setFormValues((prevValues) => ({ ...prevValues, [name]: newValue }));
+  };
   return (
     <div>
-      {/* From */}
-      <form className="max-w-lg mx-auto">
+      <form className="max-w-lg mx-auto" onSubmit={handleSubmit}>
         <div className="my-4">
           <label className="block font-medium text-gray-700">
             Claimer name
           </label>
-          <select className="block w-full p-2 border rounded">
+          <select
+            name="claimerName"
+            className="block w-full p-2 border rounded"
+            value={formValues.claimerName}
+            onChange={handleFormChange}
+          >
             <option>Employee A</option>
             <option>Employee B</option>
             <option>Employee C</option>
@@ -19,21 +89,44 @@ function Home() {
           <label className="block font-medium text-gray-700">
             Date of expense
           </label>
-          <input type="date" className="block w-full p-2 border rounded" />
+          <input
+            type="date"
+            name="expenseDate"
+            className="block w-full p-2 border rounded"
+            value={formValues.expenseDate}
+            onChange={handleFormChange}
+          />
         </div>
         <div className="my-4">
           <label className="block font-medium text-gray-700">Description</label>
-          <input type="text" className="block w-full p-2 border rounded" />
+          <textarea
+            name="description"
+            className="block w-full p-2 border rounded"
+            value={formValues.description}
+            onChange={handleFormChange}
+          />
         </div>
         <div className="my-4">
           <label className="block font-medium text-gray-700">
             Amount (EUR)
           </label>
-          <input type="number" className="block w-full p-2 border rounded" />
+          <input
+            type="number"
+            name="amount"
+            className="block w-full p-2 border rounded"
+            value={formValues.amount}
+            onChange={handleFormChange}
+          />
         </div>
         <div className="my-4">
           <label className="block font-medium text-gray-700">Approved</label>
-          <input type="checkbox" className="inline-block mr-2" />
+          <input
+            type="checkbox"
+            name="approved"
+            className="inline-block mr-2"
+            checked={formValues.approved}
+            onChange={handleFormChange}
+          />
         </div>
         <div className="my-4">
           <button
@@ -44,51 +137,8 @@ function Home() {
           </button>
         </div>
       </form>
-      {/* Table */}
-      <table className="table-auto w-full mb-12">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Claimer</th>
-            <th className="px-4 py-2">Date</th>
-            <th className="px-4 py-2">Description</th>
-            <th className="px-4 py-2">Amount (EUR)</th>
-            <th className="px-4 py-2">Approved</th>
-            <th className="px-4 py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="border px-4 py-2">Employee A</td>
-            <td className="border px-4 py-2">2022-01-01</td>
-            <td className="border px-4 py-2">Lunch with partners</td>
-            <td className="border px-4 py-2">25.00</td>
-            <td className="border px-4 py-2">Yes</td>
-            <td className="border px-4 py-2">
-              <button className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600">
-                Edit
-              </button>
-              <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 ml-2">
-                Delete
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td className="border px-4 py-2">Employee B</td>
-            <td className="border px-4 py-2">2022-01-05</td>
-            <td className="border px-4 py-2">Taxi to airport</td>
-            <td className="border px-4 py-2">30.00</td>
-            <td className="border px-4 py-2">No</td>
-            <td className="border px-4 py-2">
-              <button className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600">
-                Edit
-              </button>
-              <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 ml-2">
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <TableExpenses toggleModal={toggleModal} l />
+      {isOpen && <EditExpenseModal toggleModal={toggleModal} />}
     </div>
   );
 }
