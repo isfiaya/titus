14fcb26b071981bd665e998022ff90c39b824ed3
Aymen Isfiaya <aymen.isfiaya@gmail.com>
2023-02-19 10:16:56 +0100

    add: expenses logic

---
+++ src/logic/expensesLogic.js
@@ -0,0 +1,56 @@
+import { kea } from 'kea';
+import { supabase } from '../supabase/init';
+
+const expensesLogic = kea({
+  actions: {
+    setExpenses: (expenses) => ({ expenses }),
+    addExpense: (expense) => ({ expense }),
+    loadExpenses: true,
+    saveExpense: (expense) => ({ expense }),
+    deleteExpense: (id) => ({ id }),
+
+  },
+  reducers: {
+    expenses: [[], {
+      setExpenses: (_, { expenses }) => expenses,
+
+      addExpense: (expenses, { expense }) => [...expenses, expense],
+    }],
+  },
+  listeners: ({ actions }) => ({
+    loadExpenses: async () => {
+      const { data: expenses, error } = await supabase
+        .from('expenses')
+        .select('*');
+
+      if (error) {
+        console.error(error);
+      } else {
+        actions.setExpenses(expenses);
+      }
+    },
+    saveExpense: async ({ expense }) => {
+      const { error } = await supabase
+        .from('expenses')
+        .insert(expense);
+
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Expense inserted successfully:");
+
+      }
+    },
+    deleteExpense: async ({ id }) => {
+      const { error } = await supabase.from("expenses").delete().eq("id", id);
+      if (error) {
+        console.log("Error deleting expense:", error.message);
+      } else {
+        console.log(`Expense with ID ${id} deleted successfully.`);
+      }
+    }
+
+  }),
+});
+
+export default expensesLogic;
