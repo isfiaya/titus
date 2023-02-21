bf6a4ff47d861cf17474ef22d4520e8f1dd6a309
Aymen Isfiaya <aymen.isfiaya@gmail.com>
2023-02-21 23:24:23 +0100

    feat: improve tabla ui

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,21 +1,38 @@
+import { useActions } from "kea";
 import React, { useState } from "react";
+import expensesLogic from "../logic/expensesLogic";
+import ConfirmationModal from "./ConfirmationModal";
 
-function Table({ data, columns, pageSize = 5 }) {
+function Table({ data, columns, pageSize = 5, toggleModal }) {
   const [currentPage, setCurrentPage] = useState(1);
   const startIndex = (currentPage - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   const pageData = data.slice(startIndex, endIndex);
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+  const { setSelectedExpenseId } = useActions(expensesLogic);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
     console.log("run", page);
   };
+  const handleModalEditExpense = async (id) => {
+    toggleModal();
+    console.log("id", id);
+    setSelectedExpenseId(id);
+  };
+  const handleModalDeletExpense = async (id) => {
+    setDeleteConfirmationOpen(true);
+    setSelectedExpenseId(id);
+  };
+  const toggleModalDelete = () => {
+    setDeleteConfirmationOpen(!deleteConfirmationOpen);
+  };
 
   const pageCount = Math.ceil(data.length / pageSize);
 
   return (
-    <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div className="flex flex-col mb-8">
+      <div className=" overflow-x-auto lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
@@ -30,6 +47,16 @@ function Table({ data, columns, pageSize = 5 }) {
                       {column.label}
                     </th>
                   ))}
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Approved
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  ></th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
@@ -43,6 +70,24 @@ function Table({ data, columns, pageSize = 5 }) {
                         {row[column.key]}
                       </td>
                     ))}
+
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {row["approved"] ? "Yes" : "No"}
+                    </td>
+                    <td className="px-6 py-4 flex">
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded mr-2 ml-auto font-medium text-sm "
+                        onClick={() => handleModalEditExpense(row.id)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="bg-red-500 hover:bg-red-700 text-white  py-2 px-4 rounded font-medium text-sm"
+                        onClick={() => handleModalDeletExpense(row.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -54,7 +99,7 @@ function Table({ data, columns, pageSize = 5 }) {
         {Array.from({ length: pageCount }, (_, i) => (
           <button
             key={i}
-            className={`px-4 py-2 mr-2 rounded ${
+            className={`px-3 py-1 text-base mr-2 rounded-full ${
               i + 1 === currentPage
                 ? "bg-blue-500 text-white"
                 : "bg-gray-200 text-gray-700"
@@ -65,6 +110,9 @@ function Table({ data, columns, pageSize = 5 }) {
           </button>
         ))}
       </div>
+      {deleteConfirmationOpen && (
+        <ConfirmationModal toggleModalDelete={toggleModalDelete} />
+      )}
     </div>
   );
 }
