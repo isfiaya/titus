7075636faea89bb7135162c66ce4f1fa9d62a747
Aymen Isfiaya <aymen.isfiaya@gmail.com>
2023-02-23 02:37:32 +0100

    refactor: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useActions } from "kea";
 import { useEffect } from "react";
-import reactLogo from "./assets/react.svg";
 import expensesLogic from "./logic/expensesLogic";
 import RouterConfig from "./routing/RouterConfig";
 
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -4,15 +4,6 @@ import { useState } from "react";
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-  let activeStyle = {
-    textDecoration: "underline",
-  };
-
-  let activeClassName = "underline";
-
   return (
     <nav className="bg-blue-500 ">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -20,41 +11,21 @@ function Navbar() {
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              onClick={toggle}
+              className="p-1 rounded-full bg-white  hover:text-white border"
+              onClick={() => setIsOpen(!isOpen)}
             >
               {isOpen ? (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
+                <img
+                  src="src/assets/icons/close.svg"
+                  alt="close-btn"
+                  className="w-4"
+                />
               ) : (
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
+                <img
+                  src="src/assets/icons/menu.svg"
+                  alt="menu-open-btn"
+                  className="w-4"
+                />
               )}
             </button>
           </div>
@@ -69,16 +40,10 @@ function Navbar() {
             </div>
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
-                <NavLink
-                  to="/"
-                  className="text-white hover:bg-[#7da4e3] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
+                <NavLink to="/" className="nav-link">
                   Expenses
                 </NavLink>
-                <NavLink
-                  to="/stats"
-                  className="text-white hover:bg-[#7da4e3] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
+                <NavLink to="/stats" className="nav-link">
                   Stats
                 </NavLink>
               </div>
@@ -95,14 +60,14 @@ function Navbar() {
           <NavLink
             to="/"
             activeclassname="bg-gray-900 text-white"
-            className="text-white hover:bg-[#7da4e3] hover:text-white block px-3 py-2 rounded-md  font-medium text-sm"
+            className="nav-link"
           >
             Expenses
           </NavLink>
           <NavLink
             to="/stats"
             activeclassname="bg-gray-900 text-white"
-            className="text-white hover:bg-[#7da4e3] hover:text-white block px-3 py-2 rounded-md font-medium text-sm"
+            className="nav-link"
           >
             Stats
           </NavLink>
